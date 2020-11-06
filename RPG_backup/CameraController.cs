abd5f9f50c848a95f263988ed9803a7191948236
Kyle Lamoureux <klamoureux386@gmail.com>
2020-11-06 01:58:19 -0500

    Add files via upload

---
+++ RPG_backup/CameraController.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera playerCamera;
+    public Volume postProcessingVolume;
+    public GameObject playerObject;
+
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 75.0f;
+    public float bobLerpDownSpeed = 0.05f;
+    public float bobLerpUpSpeed = 0.03f;
+
+    //private float vignWalkLerpTime = 0.175f; //take 0.175s to lerp to walk vignette
+    //private float vignSprintLerpTime = 0.25f; //take 0.25s to lerp to sprint (enrage) vignette
+    //private float vignWalkLerpStart = 0;
+    //private float vignSprintLerpStart = 0;
+
+    public float minFallDistanceToBob = -10.0f;
+    public float maxFallDistanceBobCap = -30.0f;
+
+    private Vignette vignetteComponent;
+    private float vignLerpSpeed = 0.025f;
+    private float vignWalkIntensity = 0.25f;
+    private float vignSprintIntensity = 0.3f;
+    private Color vignWalkColor = Color.black;
+    private Color vignSprintColor = Color.red;
+
+    private Vector3 cameraRestingLocation; //SET AS LOCAL POSITION
+    private bool cameraBobDown = false;
+    private bool cameraBobUp = false;
+    private Vector3 cameraBobDownTarget = new Vector3(0, 0.0025f, 0); //magic number to use as a ratio for lowering a maximum of 0.050 units when multiplied by Y velocity
+
+    private float rotationX = 0;
+
+    void Start()
+    {
+        cameraRestingLocation = playerCamera.transform.localPosition;
+        //Debug.Log("Camera resting location: " + cameraRestingLocation);
+
+        //Get Vignette
+        Vignette tmp;
+        if (postProcessingVolume.profile.TryGet<Vignette>(out tmp))
+            vignetteComponent = tmp;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cameraBobDown && !cameraBobUp) {
+            bobCameraDown();
+        }
+
+        else if (cameraBobUp && !cameraBobDown) {
+            bobCameraUp();
+        }
+
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+    }
+
+    public void checkBobCamera(float lastJumpYPos, bool jumpToAir)
+    {
+        float fallDistance = playerObject.transform.position.y - lastJumpYPos;
+
+        if (jumpToAir)
+            fallDistance += 2;
+
+        Debug.Log("Hit ground; Fall distance: " + fallDistance);
+        Debug.Log("Fall distance to bob: " + minFallDistanceToBob);
+
+        if (fallDistance > minFallDistanceToBob || cameraBobDown || cameraBobUp) //return if fall distance not < -5 or if camera bobbing
+            return;
+
+
+        if (fallDistance < maxFallDistanceBobCap)
+        {
+            fallDistance = maxFallDistanceBobCap;
+        }
+
+        //Start downward bob
+        cameraBobDownTarget = new Vector3(0, 0.0025f, 0) * fallDistance; //magic number to use as a ratio for how far target is moved down
+        cameraBobDown = true;
+
+        Debug.Log("Start Bob");
+    }
+
+    
+    private void bobCameraDown() {
+
+        
+        float distanceToTarget = Vector3.SqrMagnitude(playerCamera.transform.localPosition - cameraBobDownTarget);
+
+        /*Debug.Log("Camera location: " + playerCamera.transform.localPosition);
+        Debug.Log("Target to bob to: " + cameraBobDownTarget);
+        Debug.Log("Distance to target: " + distanceToTarget);*/
+
+        if (distanceToTarget < 0.01) {
+            cameraBobDown = false;
+            cameraBobUp = true;
+            Debug.Log("Done bobbing down");
+        }
+
+        else
+            playerCamera.transform.localPosition = Vector3.Lerp(playerCamera.transform.localPosition, cameraBobDownTarget, bobLerpDownSpeed); //Lerps camera down to target
+            
+
+        /*
+        float timeSinceStarted = Time.time - timeBobDownStart;
+        float percentageComplete = timeSinceStarted / bobLerpDownTime;
+
+        Debug.Log("Time since started: " + timeSinceStarted);
+
+        playerCamera.transform.localPosition = Vector3.Lerp(cameraRestingLocation, cameraBobDownTarget, percentageComplete);
+
+        //Start upward bob
+        if (percentageComplete >= 1.0f) {
+            cameraBobDown = false;
+            timeBobUpStart = Time.time;
+            cameraBobUp = true;
+        }
+        */
+    }
+
+    private void bobCameraUp() {
+
+        
+        float distanceToTarget = Vector3.SqrMagnitude(playerCamera.transform.localPosition - cameraRestingLocation);
+
+        /*Debug.Log("Camera location: " + playerCamera.transform.localPosition);
+        Debug.Log("Target to bob to: " + cameraRestingLocation);
+        Debug.Log("Distance to target: " + distanceToTarget);*/
+
+        if (distanceToTarget < 0.01)
+        {
+            cameraBobUp = false;
+            Debug.Log("Done bobbing up");
+        }
+
+        else
+            playerCamera.transform.localPosition = Vector3.Lerp(playerCamera.transform.localPosition, cameraRestingLocation, bobLerpUpSpeed); //bobs back up to camera resting location
+        
+
+        /*
+        float timeSinceStarted = Time.time - timeBobUpStart;
+        float percentageComplete = timeSinceStarted / bobLerpUpTime;
+
+        playerCamera.transform.localPosition = Vector3.Lerp(cameraBobDownTarget, cameraRestingLocation, percentageComplete);
+
+        if (percentageComplete >= 1.0f)
+        {
+            cameraBobUp = false;
+        }
+        */
+
+    }
+
+    //https://forum.unity.com/threads/hdrp-2019-1-how-do-you-change-effects-volume-overrides-through-script.668842/
+    //http://www.blueraja.com/blog/404/how-to-use-unity-3ds-linear-interpolation-vector3-lerp-correctly
+    public void setSprintVignette() {
+
+        //cancel if vignette the same
+        if (vignetteComponent.color == vignSprintColor && vignetteComponent.intensity == vignSprintIntensity) {
+            //Debug.Log("Vignette equal to sprint color/intensity");
+            return;
+        }
+
+        //Debug.Log("Changing Vignette to sprint color/intensity");
+
+        //if current color is similar to target color within 1 rgb, set to target color
+        if (compareSimilarColors(vignetteComponent.color.value, vignSprintColor)) {
+            vignetteComponent.color.value = vignSprintColor;
+            vignetteComponent.intensity.value = vignSprintIntensity;
+        }
+
+        else
+        {
+            vignetteComponent.color.Interp(vignetteComponent.color.value, vignSprintColor, vignLerpSpeed);
+            vignetteComponent.intensity.Interp(vignetteComponent.intensity.value, vignSprintIntensity, vignLerpSpeed);
+        }
+
+    }
+
+    public void setWalkVignette(){
+
+        if (vignetteComponent.color == vignWalkColor && vignetteComponent.intensity == vignWalkIntensity) {
+            //Debug.Log("Vignette equal to walk color/intensity");
+            return;
+        }
+
+        //Debug.Log("Changing Vignette to walk color/intensity");
+
+        //if current color is similar to target color within 1 rgb, set to target color
+        if (compareSimilarColors(vignetteComponent.color.value, vignWalkColor)) {
+            vignetteComponent.color.value = vignWalkColor;
+            vignetteComponent.intensity.value = vignWalkIntensity;
+        }
+
+        //else interpolate
+        else
+        {
+            vignetteComponent.color.Interp(vignetteComponent.color.value, vignWalkColor, vignLerpSpeed);
+            vignetteComponent.intensity.Interp(vignetteComponent.intensity.value, vignWalkIntensity, vignLerpSpeed);
+        }
+    }
+
+    //https://answers.unity.com/questions/787056/comparing-2-color-variables.html
+    private bool compareSimilarColors(Color color1, Color color2) {
+
+        int decMultiplier = 1000; //multiply by 1000 to show get 3 sig figs
+        int margin = 1;
+        //this makes margin essentially = 0.001 on a scale of [0, 1] for normal colors
+
+        float color1r = color1.r * decMultiplier;
+        float color1g = color1.g * decMultiplier;
+        float color1b = color1.b * decMultiplier;
+        float color1a = color1.a * decMultiplier;
+        float color2r = color2.r * decMultiplier;
+        float color2g = color2.g * decMultiplier;
+        float color2b = color2.b * decMultiplier;
+        float color2a = color2.a * decMultiplier;
+
+
+        Debug.Log("Red Colors: " + color1r + ", " + color2r + "\n" +
+            "Green Colors: " + color1g + ", " + color2g + "\n" +
+            "Blue Colors: " + color1b + ", " + color2b + "\n");
+
+        if (Mathf.Abs(color1r - color2r) <= margin &&
+            Mathf.Abs(color1g - color2g) <= margin &&
+            Mathf.Abs(color1b - color2b) <= margin &&
+            Mathf.Abs(color1a - color2a) <= margin) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ RPG_backup/PlayerController.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement Variables
+    private float walkSpeed = 16.0f;
+    private float runSpeed = 24.0f;
+    private float jumpSpeed = 18.0f;
+    private float airDriftSpeed = 0.5f; //for left-right movement in air
+    private float gravity = 30.0f;
+
+    //Slide Variables
+    private float slideMultiplier = 1.2f;
+    private float timeForcedToSlide = 1.5f;
+    private float timeSlideStart = 0;
+
+    //Movement States
+    private bool crouching = false;
+    private bool sliding = false;
+    private bool isRunning = false;
+    private bool forcedToSlide = false;
+
+    private bool airJumpUsed = false;
+    private bool runningWhenJumped = false;
+    private bool lastInAir = false;
+    private bool jumpToAir = false; //bool to determine if we jumped to end up in air or walked off of a ledge
+
+    //Movement Vectors
+    private Vector3 lastPosition;
+    private Vector3 positionOfLastJump;
+    private Vector3 lastMomentum;
+    private Vector3 slideMomentum;
+
+    public GameObject playerObject;
+    CameraController cameraController;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cameraController = GetComponent<CameraController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        lastPosition = playerObject.transform.position;
+        positionOfLastJump = playerObject.transform.position;
+    }
+
+    void Update()
+    {
+
+
+        lastPosition = playerObject.transform.position;
+
+
+        //Debug.Log(characterController.isGrounded ? "GROUNDED" : "NOT GROUNDED");
+
+        if (!crouching && Input.GetButton("Shift"))
+        {
+            isRunning = true;
+        }
+
+        else if (!sliding && characterController.isGrounded)
+        {
+            isRunning = false;
+        }
+
+        if (Input.GetButton("Crouch") || forcedToSlide)
+        {
+            //Debug.Log("Crouching");
+            crouching = true;
+        }
+
+        else {
+            crouching = false;
+            sliding = false;
+            Debug.Log("Not Sliding");
+        }
+
+        /*
+        if (isRunning)
+            cameraController.setSprintVignette();
+
+        else
+            cameraController.setWalkVignette();
+        */
+
+        if (characterController.isGrounded) {
+
+            //if last frame was in air, and we just hit ground, bob camera based on fall speed
+            if (lastInAir) {
+                ///Debug.Log("y position of last jump: " + positionOfLastJump.y);
+                cameraController.checkBobCamera(positionOfLastJump.y, jumpToAir);
+            }
+
+            airJumpUsed = false;
+            lastInAir = false;
+            positionOfLastJump = playerObject.transform.position; //set to player location for cases where character walks off cliff
+
+            //can only slide if moving forward and currently running.
+            if (isRunning && Input.GetButton("Crouch") && Input.GetAxis("Vertical") > 0 && !sliding)
+            {
+                Debug.Log("Begin Sliding");
+                sliding = true;
+                timeSlideStart = Time.time;
+                StartCoroutine(forceSlideForTime(timeForcedToSlide));
+                startSlide();
+            }
+
+            if (sliding)
+            {
+                Debug.Log("Sliding");
+                //slideDecay();
+
+                if (sliding && Input.GetButtonDown("Jump") && !forcedToSlide)
+                {
+                    Debug.Log("Jumping from slide");
+                    groundJump();
+                    sliding = false;
+                }
+
+            }
+
+            //if not sliding
+            else
+            {
+
+                //adds slight downward push to ensure isGrounded is detected properly
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), -0.01f, Input.GetAxis("Vertical"));
+                moveDirection = transform.TransformDirection(moveDirection);
+
+                if (moveDirection.magnitude > 1)
+                {
+                    moveDirection.Normalize();
+                }
+
+                //if running, multiply moveDirection by runSpeed, else multiply by walk speed
+                moveDirection *= (isRunning ? runSpeed : walkSpeed);
+                runningWhenJumped = isRunning;
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    groundJump();
+                }
+
+            }
+
+        }
+
+        else {
+            lastInAir = true;
+
+            //if jumping in air
+            if (Input.GetButtonDown("Jump") && !airJumpUsed)
+            {
+                airJump();
+            }
+
+            //For drifting in air
+            else {
+
+                //To do: ?
+            }
+
+            //apply gravity if in air
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        }
+
+        lastMomentum = moveDirection;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void startSlide() {
+
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), -0.01f, Input.GetAxisRaw("Vertical"));
+        if (moveDirection.magnitude > 1)
+        {
+            moveDirection.Normalize();
+        }
+        moveDirection.x *= (runSpeed * slideMultiplier);
+        moveDirection.z *= (runSpeed * slideMultiplier);
+
+        slideMomentum = moveDirection; //BEFORE TRANSFORM DIRECTION
+
+        moveDirection = transform.TransformDirection(moveDirection);
+    }
+
+    private void slideDecay() {
+
+        float decayAmount = 0.1f;
+
+        if (moveDirection.x > 0 && moveDirection.x < decayAmount)
+            moveDirection.x = 0;
+
+        else
+            moveDirection.x -= decayAmount;
+
+        if (moveDirection.z > 0 && moveDirection.z < decayAmount)
+            moveDirection.z = 0;
+
+        else
+            moveDirection.z -= decayAmount;
+
+        //moveDirection = transform.TransformDirection(moveDirection);
+
+    }
+
+    private void groundJump() {
+        moveDirection.y = jumpSpeed;
+        positionOfLastJump = playerObject.transform.position;
+        jumpToAir = true;
+    }
+
+    private void airJump() {
+
+        airJumpUsed = true;
+        jumpToAir = true;
+        positionOfLastJump = playerObject.transform.position;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (moveDirection.magnitude > 1)
+        {
+            moveDirection.Normalize();
+        }
+
+        moveDirection *= (runningWhenJumped ? runSpeed : walkSpeed);
+        moveDirection.y = jumpSpeed * 0.75f; //slightly lower second jump
+
+        //reset bool for next jump
+        runningWhenJumped = false;
+    }
+
+    private IEnumerator forceSlideForTime(float seconds) {
+
+        forcedToSlide = true;
+        Debug.Log("Forced to slide");
+
+        yield return new WaitForSeconds(seconds);
+
+        forcedToSlide = false;
+        crouching = Input.GetButton("Crouch");
+
+        Debug.Log("Not forced to slide");
+
+        yield return null;
+    }
+}
