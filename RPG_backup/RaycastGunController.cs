6a8fdf78570b0903a57a4bb8510ac1e0324aa041
Kyle Lamoureux <klamoureux386@gmail.com>
2020-11-28 04:55:32 -0500

    last upload before playerController rewrite

--- RPG_backup/RaycastGunController.cs
+++ RPG_backup/RaycastGunController.cs
@@ -60,11 +60,43 @@ public class RaycastGunController : MonoBehaviour
     //https://forum.unity.com/threads/multiple-event-burst-spawns-of-the-same-vfxgraph-possible.852049/
     private void spawnImpactEffect(Vector3 pointOfImpact, Vector3 impactPointNormal) {
 
+        Vector3 velocityMins = new Vector3(-1.0f, -1.0f, -1.0f);
+        Vector3 velocityMaxs = new Vector3(1.0f, 1.0f, 1.0f);
+
+        //if normal value is negative, add it to the max (reduces max by abs)
+        //if normal value is positive, add it to the min
+
+        if (impactPointNormal.x <= 0)
+            velocityMaxs.x += impactPointNormal.x;
+
+        else
+            velocityMins.x += impactPointNormal.x;
+
+        if (impactPointNormal.y <= 0)
+            velocityMaxs.y += impactPointNormal.y;
+
+        else
+            velocityMins.y += impactPointNormal.y;
+
+        if (impactPointNormal.z <= 0)
+            velocityMaxs.z += impactPointNormal.z;
+
+        else
+            velocityMins.z += impactPointNormal.z;
+
         impactEventAttribute.SetVector3("targetPosition", pointOfImpact);
-        impactEventAttribute.SetVector3("angle", impactPointNormal);
+
+        impactEventAttribute.SetVector3("direction", velocityMins); //min velocity = direction
+        impactEventAttribute.SetVector3("velocity", velocityMaxs); //max velocity = velocity
+
         impactEffect.SendEvent("OnSpawn", impactEventAttribute);
 
+        /*
         Debug.Log("position to spawn impact at: " + pointOfImpact);
+        Debug.Log("impact point normal: " + impactPointNormal);
+        Debug.Log("Velocity mins: " + velocityMins);
+        Debug.Log("Velocity maxs: " + velocityMaxs);
+        */
 
     }
 }
